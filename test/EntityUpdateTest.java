86756a1d73f44819865e0666a1e21cb82f33e702
p10614040 <695795023@qq.com>
2015-08-11 09:01:46 +0800

    Hibernate测试代码提交

---
+++ test/EntityUpdateTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @Title EntityUpdateTest.java
+ * @Package 
+ * @Description TODO(用一句话描述该文件做什么)
+ * @author yan.liang
+ * @date 2015年8月3日 下午10:05:47
+ * @copyright USTCINFO
+ */
+
+/**
+ * @author YanLiang
+ *
+ */
+public class EntityUpdateTest {
+
+	/**
+	 * @author yan.liang
+	 * @date 2015年8月3日 下午10:05:47
+	 * @Description TODO(这里用一句话描述这个方法的作用)
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @author yan.liang
+	 * @date 2015年8月3日 下午10:05:47
+	 * @Description TODO(这里用一句话描述这个方法的作用)
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
